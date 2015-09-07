68212ba01867e613bc98cb4d94db4a212c88faa6
oga <oga.ivc.s27@gmail.com>
2015-09-07 21:33:08 +0900

    merged static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#index'
+
+  get   'about'   => 'static_pages#about'
+  get   'news'    => 'static_pages#news'
+  get   'rules'   => 'static_pages#rules'
+  get   'how_to_vote' => 'static_pages#how_to_vote'
+  get   'flow_chart'  => 'static_pages#flow_chart'
+  get   'next'        => 'static_pages#next'
+
   scope :contestants do
     get   'group/:id'   => 'contestants#index'
     get   'entry'       => 'contestants#entry'
