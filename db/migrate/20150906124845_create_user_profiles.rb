a900967ed76fa95cbd67299ae0f0eae1a6ca1a53
oga <oga.ivc.s27@gmail.com>
2015-09-07 12:56:08 +0900

    create UserProfile model and its migration.

---
+++ db/migrate/20150906124845_create_user_profiles.rb
@@ -0,0 +1,15 @@
+class CreateUserProfiles < ActiveRecord::Migration
+  def change
+    create_table :user_profiles do |t|
+      t.integer   :user_id,   :null => false
+      t.string    :nickname,  :null => false
+      t.integer   :sex,       :null => false
+      t.string    :age,       :null => false
+      t.string    :prefecture,:null => false
+
+      t.timestamps null: false
+    end
+
+    add_index :user_profiles, :user_id, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150906124310) do
+ActiveRecord::Schema.define(version: 20150907035206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "user_profiles", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.string   "nickname",   null: false
+    t.integer  "sex",        null: false
+    t.string   "age",        null: false
+    t.string   "prefecture", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_profiles", ["user_id"], name: "index_user_profiles_on_user_id", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                        null: false
@@ -30,4 +42,5 @@ ActiveRecord::Schema.define(version: 20150906124310) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
 
+  add_foreign_key "user_profiles", "users", on_update: :cascade, on_delete: :cascade
 end
