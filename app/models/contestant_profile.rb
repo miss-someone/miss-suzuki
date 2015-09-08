c8e08145b89ef6026f258c723611f117be85dec8
oga <oga.ivc.s27@gmail.com>
2015-09-09 02:47:29 +0900

    add validation to ContestantProfile model.

--- app/models/contestant_profile.rb
+++ app/models/contestant_profile.rb
@@ -1,3 +1,17 @@
 class ContestantProfile < ActiveRecord::Base
   belongs_to :user
+
+  # バリデーション
+  validates :user_id, presence: true
+  validates :group_id, presence: true, inclusion: { in: 1..3 }
+  validates :name, presence: true
+  validates :hurigana, presence: true
+  validates :image_url, presence: true
+  validates :age, presence: true
+  validates :height, presence: true
+  validates :come_from, presence: true
+  validates :link_url, presence: true
+  validates :comment, presence: true
+  validates :thanks_comment, presence: true
+  validates :link_url, presence: true
 end
