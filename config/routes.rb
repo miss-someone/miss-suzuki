83d5c0b9c994afef2d8207d658851b02e0a68b64
Shogo Ono <shogo_chicago@yahoo.co.jp>
2015-09-12 02:10:05 +0900

    フロント修正

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     get   'group/:id'   => 'contestants#index'
     get   'entry'       => 'contestants#entry'
     get   'new'         => 'contestants#new'
+    get   'thankyou'    => 'contestants#thankyou'
     post  'create'      => 'contestants#create'
     post  '/:id/vote'   => 'contestants#vote', as: :vote
   end
