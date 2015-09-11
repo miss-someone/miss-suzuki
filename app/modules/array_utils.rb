a50cf7ab561143d21e3544a3629b2f4de1d87b96
oga <oga.ivc.s27@gmail.com>
2015-09-11 23:10:12 +0900

    fix array util's bug

--- app/modules/array_utils.rb
+++ app/modules/array_utils.rb
@@ -1,7 +1,8 @@
 module ArrayUtils
   class << Array
     def split3(a)
-      array = a.each_slice(calc_split_size(a)).to_a
+      split_size = calc_split3_size(a)
+      array = a.each_slice(split_size).to_a
       case array.size
       when 0 then Array.new(3, [])
       when 1, 2 then array + Array.new(3 - array.size, [])
@@ -14,9 +15,14 @@ module ArrayUtils
 
     # 3分割する際の, each_sliceの引数を計算する
     def calc_split3_size(a)
-      case a.size % 3
-      when 1, 3 then a.size / 3
-      when 2 then a.size / 4 # (3 + 1)
+      size =  case a.size % 3
+              when 1, 3 then a.size / 3
+              when 2 then a.size / 3 + 1 # (3 + 1)
+              end
+      if size != 0
+        size
+      else
+        1
       end
     end
   end
