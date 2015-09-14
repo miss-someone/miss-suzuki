88dccd17243f1430a2f2f379028eea8a544af18e
oga <oga.ivc.s27@gmail.com>
2015-09-14 17:50:03 +0900

    出場者の年齢を必須じゃないようにした．未入力の場合は'なし'で埋める

--- app/models/contestant_profile.rb
+++ app/models/contestant_profile.rb
@@ -9,11 +9,21 @@ class ContestantProfile < ActiveRecord::Base
   validates :hurigana, presence: true, length: { maximum: 100 }
   validates :profile_image, presence: true
   validates :link_url, allow_blank: true, format: URI.regexp(%w(http https))
-  validates :age, presence: true, length: { maximum: 50 }
+  validates :age, length: { maximum: 50 }
   validates :height, presence: true, length: { maximum: 50 }
   validates :come_from, presence: true, length: { maximum: 50 }
   validates :comment, presence: true, length: { maximum: 100 }
   validates :thanks_comment, presence: true, length: { maximum: 100 }
   validates :is_interest_in_idol_group, presence: true
   validates :is_share_with_twitter_ok, presence: true
+
+  before_save :prepare_validation
+
+  def prepare_validation
+    if age.blank?
+      # 年齢が未入力の場合はないしょで埋める
+      self.age = 'ないしょ'
+    end
+    self
+  end
 end
