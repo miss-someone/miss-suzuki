9d3ab8cc11b0e9e072d58b160b0bbd3083ca6a32
Shogo Ono <shogo_chicago@yahoo.co.jp>
2015-09-14 23:39:01 +0900

    ・JSの実装 ・assets/imagesフォルダの整理

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,25 +16,3 @@
 //= require cropper
 //= require jquery-validation
 //= require_tree .
-
-$(function(){
-    $(".menu").click(function(){
-        $(".menu1").toggleClass("menuclick1")
-    });
-    $(".menu").click(function(){
-        $(".menu2").toggleClass("menuclick2")
-    });
-    $(".menu").click(function(){
-        $(".menu3").toggleClass("menuclick3")
-    }); 
-});
- 
-$(function(){
-            $('.menu').click(function(){
-             if ($('ul').css('display') == 'none') {
-            $('ul').slideDown('slow');
-            } else {
-             $('ul').slideUp('slow');
-             }
-            });
-        });
