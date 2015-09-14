29887f47f28b74af1d55df258cf0ebeb494f6110
Shogo Ono <shogo_chicago@yahoo.co.jp>
2015-09-14 20:28:22 +0900

    投票ページとcompletedのデザイン

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -10,6 +10,7 @@ class ContestantsController < ApplicationController
     @contestant.profile = ContestantProfile.new
   end
 
+
   def create
     # TODO: うまい方法を考えてリファクタ
     is_agree = contestant_params[:agreement]
