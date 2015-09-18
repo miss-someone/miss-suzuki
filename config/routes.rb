3deb765879000885968a875ba208b32ea688eb0b
oga <oga.ivc.s27@gmail.com>
2015-09-18 11:39:43 +0900

    activeadminのglobalNavにまつわる問題およびルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,33 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
-
-  get   'about'   => 'static_pages#about'
-  get   'news'    => 'static_pages#news'
-  get   'rules'   => 'static_pages#rules'
-  get   'how_to_vote' => 'static_pages#how_to_vote'
-  get   'flow_chart'  => 'static_pages#flow_chart'
-  get   'next'        => 'static_pages#next'
-  get   'entrypolicy'    => 'static_pages#entrypolicy'
-  get   'terms'    => 'static_pages#terms'
-  get   'policy'   => 'static_pages#policy'
-
-  scope :contestants do
-    get   'entry'       => 'contestants#entry'
-    get   'new'         => 'contestants#new'
-    get   'thankyou'    => 'contestants#thankyou'
-    get   'mypage'      => 'contestants#mypage'
-    post  'create'      => 'contestants#create'
-    if Rails.env.development?
-      get   'group/:id'   => 'contestants#index'
-      post  '/:id/vote'   => 'contestants#vote', as: :vote
-    end
-  end
-
-  # 管理画面用
   if ENV['IS_ADMIN_WEB'] == 'true'
+    # 管理画面用
     devise_for :admin_users, ActiveAdmin::Devise.config
     ActiveAdmin.routes(self)
+    root 'admin/dashboard#index'
+  else
+    # 外部向け用
+    root 'static_pages#index'
+
+    get   'about'   => 'static_pages#about'
+    get   'news'    => 'static_pages#news'
+    get   'rules'   => 'static_pages#rules'
+    get   'how_to_vote' => 'static_pages#how_to_vote'
+    get   'flow_chart'  => 'static_pages#flow_chart'
+    get   'next'        => 'static_pages#next'
+    get   'entrypolicy'    => 'static_pages#entrypolicy'
+    get   'terms'    => 'static_pages#terms'
+    get   'policy'   => 'static_pages#policy'
+
+    scope :contestants do
+      get   'entry'       => 'contestants#entry'
+      get   'new'         => 'contestants#new'
+      get   'thankyou'    => 'contestants#thankyou'
+      get   'mypage'      => 'contestants#mypage'
+      post  'create'      => 'contestants#create'
+      if Rails.env.development?
+        get   'group/:id'   => 'contestants#index'
+        post  '/:id/vote'   => 'contestants#vote', as: :vote
+      end
+    end
   end
 end
