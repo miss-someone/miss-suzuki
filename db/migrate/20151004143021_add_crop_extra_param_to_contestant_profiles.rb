cf55942ff95acf3328593bf5e9f2c5c9c15a0d0e
oga <oga.ivc.s27@gmail.com>
2015-10-05 01:18:36 +0900

    blurの具合を変える為のパラメタ用カラム追加

--- db/migrate/20151004143021_add_crop_extra_param_to_contestant_profiles.rb
+++ db/migrate/20151004143021_add_crop_extra_param_to_contestant_profiles.rb
@@ -1,5 +1,6 @@
 class AddCropExtraParamToContestantProfiles < ActiveRecord::Migration
   def change
     add_column :contestant_profiles, :profile_image_crop_param_extra, :string, null: false, default: ''
+    add_column :contestant_profiles, :profile_image_blur_param, :integer, null: false, default: 0
   end
 end
