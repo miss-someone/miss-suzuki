fa62abb50d4ffa54b4fe2480d0d78dc1f1485e5d
oga <oga.ivc.s27@gmail.com>
2015-10-17 00:21:37 +0900

    gmailの+を用いたエイリアスに対するバリデーション修正とテスト追加
    .を用いたエイリアスのテストも追加

---
+++ spec/factories/voters.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define do
+  factory :voter do
+    email "suzuki@miss-someone.com"
+    password "hogehoge"
+    user_type Settings.user_type[:normal]
+
+    factory :voter2 do
+      email "suzuki2@miss-someone.com"
+      password "hogehoge"
+    end
+
+    factory :dot_alias1 do
+      email "mi.ss-su.zu.ki@gmail.com"
+    end
+    factory :dot_alias2 do
+      email "m.iss-s.uzu.ki@gmail.com"
+    end
+  end
+end
---
+++ spec/models/voter_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Voter, type: :model do
+  let(:voter) { create(:voter) }
+
+  describe "in validation" do
+    let(:voter2) { build(:voter2) }
+
+    describe 'for email' do
+      context 'when email is valid' do
+        it { expect(voter2).to be_valid() }
+      end
+      context 'when email is nil' do
+        before { voter2.email = nil }
+        it { expect(voter2).not_to be_valid(:email) }
+      end
+      context 'when email contains "+"' do
+        before { voter2.email = "suzuki1+alias@gmail.com" }
+        it { expect(voter2).not_to be_valid(:email) }
+      end
+      context 'when email is already registered' do
+        before { voter2.email = voter.email }
+        it { expect(voter2).not_to be_valid(:email) }
+      end
+      context 'when email is already registered with gmail dots alias' do
+        let(:alias1) { create(:dot_alias1) }
+        let(:alias2) { build(:dot_alias2) }
+        it { expect(alias2).not_to be_valid(:email) }
+      end
+    end
+  end
+end
