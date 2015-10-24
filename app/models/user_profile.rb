97da3bc680b4e5272ef70192cf29168d7a56fa3a
oga <oga.ivc.s27@gmail.com>
2015-10-25 01:14:59 +0900

    UserProfileへバリデーションの追加

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -9,4 +9,10 @@ class UserProfile < ActiveRecord::Base
   # 都道府県扱うgem
   include JpPrefecture
   jp_prefecture :prefecture_code, method_name: :pref
+
+  validates :nickname, presence: true, length: { maximum: 15 }
+  validates :sex, presence: true, inclusion: { in: [Settings.sex[:male], Settings.sex[:female]] }
+  validates :age_id, presence: true, inclusion: { in: Age.first.id..Age.last.id }
+  validates :prefecture_code, presence: true, inclusion: { in: Prefecture.all.each_with_object([]){ |p, obj| obj << p.code } }
+  validates :job_id, presence: true, inclusion: { in: Job.first.id..Job.last.id }
 end
