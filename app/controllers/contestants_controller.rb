4686ca9f21d4369aa6c6eaf6cb612b9c145982ac
K0k1Nakamura <k00ook17@gmail.com>
2015-10-26 09:59:52 +0900

    微修正

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -41,16 +41,20 @@ class ContestantsController < ApplicationController
     @interview_answers = InterviewTopic.all.each_with_object([]) do |topic, res|
       res << InterviewAnswer.new(user_id: current_user.id, interview_topic_id: topic.id)
     end
-    InterviewAnswer.transaction do
-      params[:interview_answers].each do |interview_answer|
-        current_user.interview_answers.create!(interview_topic_id: interview_answer[:interview_topic_id], answer: interview_answer[:answer], is_pending: false) unless interview_answer[:answer].blank?
+    begin
+      InterviewAnswer.transaction do
+        params[:interview_answers].each do |interview_answer|
+          current_user.interview_answers.create!(interview_topic_id: interview_answer[:interview_topic_id],
+                                                 answer: interview_answer[:answer],
+                                                 is_pending: false) unless interview_answer[:answer].blank?
+        end
       end
+      flash.now.alert = "インタビューの回答の登録に成功しました。"
+      render 'new_interview_answer'
+    rescue
+      flash.now.alert = "インタビューの回答の登録に失敗しました（インタビューの回答は200文字までです）。"
+      render 'new_interview_answer'
     end
-    flash.now.alert = "インタビューの回答の登録に成功しました。"
-    render 'new_interview_answer'
-    rescue => e
-    flash.now.alert = "インタビューの回答の登録に失敗しました（インタビューの回答は200文字までです）。"
-    render 'new_interview_answer'
   end
 
   private
@@ -59,7 +63,7 @@ class ContestantsController < ApplicationController
     params.require(:contestant).permit(:email, :password, :agreement,
                                        { contestant_tag_ids: [] },
                                        contestant_profile_attributes:
-                                        [:name, :hurigana, :age, :come_from, :comment,
+                                       [:name, :hurigana, :age, :come_from, :comment,
                                          :link_url, :thanks_comment, :height,
                                          :profile_image, :email, :password, :phone,
                                          :station, :is_interest_in_idol_group,
@@ -67,8 +71,8 @@ class ContestantsController < ApplicationController
                                          :profile_image_crop_param_x, :profile_image_crop_param_y,
                                          :profile_image_crop_param_height,
                                          :profile_image_crop_param_width
-                                        ],
-                                       interview_answers_attributes: [:user, :interview_topic_id, :answer]
+    ],
+      interview_answers_attributes: [:user, :interview_topic_id, :answer]
                                       )
   end
 end
