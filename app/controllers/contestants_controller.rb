4797a614960406eccb629bbaa96ff710c701e603
K0k1Nakamura <k00ook17@gmail.com>
2015-10-26 19:31:17 +0900

    rubocopに怒られたところ

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -55,15 +55,15 @@ class ContestantsController < ApplicationController
                                        { contestant_tag_ids: [] },
                                        contestant_profile_attributes:
                                        [:name, :hurigana, :age, :come_from, :comment,
-                                         :link_url, :thanks_comment, :height,
-                                         :profile_image, :email, :password, :phone,
-                                         :station, :is_interest_in_idol_group,
-                                         :how_know, :is_share_with_twitter_ok,
-                                         :profile_image_crop_param_x, :profile_image_crop_param_y,
-                                         :profile_image_crop_param_height,
-                                         :profile_image_crop_param_width
-    ],
-      interview_answers_attributes: [:user, :interview_topic_id, :answer]
+                                        :link_url, :thanks_comment, :height,
+                                        :profile_image, :email, :password, :phone,
+                                        :station, :is_interest_in_idol_group,
+                                        :how_know, :is_share_with_twitter_ok,
+                                        :profile_image_crop_param_x, :profile_image_crop_param_y,
+                                        :profile_image_crop_param_height,
+                                        :profile_image_crop_param_width
+                                       ],
+                                       interview_answers_attributes: [:user, :interview_topic_id, :answer]
                                       )
   end
 end
