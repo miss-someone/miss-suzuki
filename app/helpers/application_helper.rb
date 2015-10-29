87dd99f76e290a01a8bc95945c8fb97f1eb3665b
K0k1Nakamura <k00ook17@gmail.com>
2015-10-30 02:18:12 +0900

    インタビュー未回答時にインタビュー欄を非表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
 
   def show_my_own_page
     return unless current_user
-    return unless current_user.user_type != 2
+    return unless current_user.user_type == 2
     '<li class="tab"><a href="/contestants/my_own_page">MY PAGE<br><span>マイページ</span></a></li>'
   end
 
