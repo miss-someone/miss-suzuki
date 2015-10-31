998fd5b528f57bf4abe8d33c3d73b4c797288343
oga <oga.ivc.s27@gmail.com>
2015-10-31 20:40:38 +0900

    GAタグが削除されてしまっていたので戻す

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # replace this with your tracker code
+  GA.tracker = "UA-67558174-1"
 end
