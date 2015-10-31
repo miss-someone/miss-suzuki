c925186667b63751ae9962d234cc1c2ac62f9fc4
oga <oga.ivc.s27@gmail.com>
2015-10-31 19:13:36 +0900

    removed name space

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-      config.redis = { url: 'redis://localhost:6379/', namespace: "sidekiq_miss-suzuki_#{Rails.env}" }
+      config.redis = { url: 'redis://localhost:6379/' }
 end
 
 Sidekiq.configure_client do |config|
-      config.redis = { url: 'redis://localhost:6379/', namespace: "sidekiq_miss-suzuki_#{Rails.env}" }
+      config.redis = { url: 'redis://localhost:6379/' }
 end 
