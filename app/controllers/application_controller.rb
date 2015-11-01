af42ad5b50b3940208033a96b570295715e48eab
oga <oga.ivc.s27@gmail.com>
2015-11-01 18:07:57 +0900

    管理画面ではDeviseの認証機能を用いるため，sorceryの認証はスキップ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :require_login
+  
+  # 管理画面ではDeviseの認証機能を用いるのでスキップ
+  before_filter :require_login unless ENV['IS_ADMIN_WEB'] == 'true'
 
   private
 
