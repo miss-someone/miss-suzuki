3b827a6262dc2e5f64ea04ca84329b556b65fcf6
oga <oga.ivc.s27@gmail.com>
2015-11-03 00:22:56 +0900

    ContestantImageのアップロードジョブをバックグラウンドで行うよう変更

--- app/models/contestant_image.rb
+++ app/models/contestant_image.rb
@@ -1,6 +1,8 @@
 class ContestantImage < ActiveRecord::Base
   belongs_to :user
   mount_uploader :profile_image, ContestantProfileImageUploader
+  store_in_background :profile_image
+
   validates :profile_image, presence: true
   validates :is_pending, inclusion: { in: [true, false] }
 
