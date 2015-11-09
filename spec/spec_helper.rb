a78f507f49141bddbbf327d8306d854668cbca9f
oga <oga.ivc.s27@gmail.com>
2015-11-09 10:25:07 +0900

    カバレッジ測定ツール追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,8 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'factory_girl_rails'
+require 'simplecov'
+SimpleCov.start
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
