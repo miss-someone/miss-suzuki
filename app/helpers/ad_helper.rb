e28c24e280c04787c84228158a43f06648fd95ae
oga <oga.ivc.s27@gmail.com>
2015-11-13 01:22:34 +0900

    fix syntax

--- app/helpers/ad_helper.rb
+++ app/helpers/ad_helper.rb
@@ -2,6 +2,6 @@ module AdHelper
   def ad_img
     ad = Ad.active.random
     return if ad.blank?
-    link_to (image_tag ad.image.url), ad.link_url, target: '_blank'
+    link_to((image_tag ad.image.url), ad.link_url, target: '_blank')
   end
 end
