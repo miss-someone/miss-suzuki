b44539c743e2d49d8d5893a97d2d5ddaa10cb9f0
oga <oga.ivc.s27@gmail.com>
2015-11-20 21:50:22 +0900

    エラー管理のerrbit追加

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,14 @@
+require 'rake'
+require 'airbrake/rake_handler'
+if Rails.env.production?
+  Airbrake.configure do |config|
+    config.api_key = ENV['ERRBIT_API_KEY']
+    config.host    = ENV['ERRBIT_API_HOST']
+    config.port    = ENV['ERRBIT_API_PORT']
+    config.development_environments = []
+    config.secure  = config.port == 443
+
+    # report exceptions that happen inside a rake task
+    config.rescue_rake_exceptions = true
+  end
+end
