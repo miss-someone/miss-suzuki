8aef5aa7a4daa289a736feda57300d95a2541295
oga <oga.ivc.s27@gmail.com>
2015-11-21 11:07:48 +0900

    thanksページヘ広告追加．及び，広告表示をヘルパーメソッドに

--- app/helpers/ad_helper.rb
+++ app/helpers/ad_helper.rb
@@ -4,4 +4,13 @@ module AdHelper
     return if ad.blank?
     link_to((image_tag ad.image.url), ad.link_url, target: '_blank')
   end
+
+  def adsense(ad_name=nil)
+    ad = Adsense.where(name: ad_name).first
+    if ad.present?
+      ad.code.html_safe
+    else
+      ad_img
+    end
+  end
 end
