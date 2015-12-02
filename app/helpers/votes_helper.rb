7b88908fab8b1f545446314026d276d74d64c836
oga <oga.ivc.s27@gmail.com>
2015-12-02 13:54:30 +0900

    投票数超過時の文言修正

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,9 +1,9 @@
 module VotesHelper
   def cannot_vote_text(group_id)
     if logged_in?
-      "ごめんなさい！今日はもう第#{group_id}グループに10回投票しちゃったみたいです・・・<br>他のグループには投票しましたか？".html_safe
+      "ごめんなさい！今日はもう10回投票しちゃったみたいです・・・".html_safe
     else
-      "ごめんなさい！ゲストユーザーさんの投票回数は1グループにつき1日3回までみたいです・・・<br>他のグループには投票しましたか？".html_safe
+      "ごめんなさい！ゲストユーザーさんの投票回数は1日3回までみたいです・・・".html_safe
     end
   end
 end
