dc2b3f1f338534fb4c2b472a7823421ee3299b2c
oga <oga.ivc.s27@gmail.com>
2015-12-04 13:25:18 +0900

    DBに記録されたdeny domainにもとづいて，登録を拒否する処理を追加

--- app/models/email_domain.rb
+++ app/models/email_domain.rb
@@ -2,4 +2,11 @@ class EmailDomain < ActiveRecord::Base
   validates :domain, presence: true
   validates :is_forbidden, inclusion: { in: [true, false] }
   validates :is_notified, inclusion: { in: [true, false] }
+
+  class << self
+    def denied?(email)
+      email_domain = email.sub(/.+@/, '')
+      where(domain: email_domain, is_forbidden: true).exists?
+    end
+  end
 end
