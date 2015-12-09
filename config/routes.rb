7eed8eb432aba5a1e4091e581f883d0d35238f2b
Shogo Ono <shogo_chicago@yahoo.co.jp>
2015-12-09 10:56:15 +0900

    鈴木社長の記事　後編

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
     get   'contents/erai_suzukisan1_2' => 'contents#interview1_2'
     get   'contents/erai_suzukisan_sp1' => 'contents#interview_sp1'
     get   'contents/erai_suzukisan2_1' => 'contents#interview2_1'
+    get   'contents/erai_suzukisan2_2' => 'contents#interview2_2'
     get   'news' => 'news#index'
 
     scope :contestant_image do
