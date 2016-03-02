29788254343993800e8d2bd02fb1d5e22bdc812e
Shogo Ono <shogo_chicago@yahoo.co.jp>
2016-03-02 22:53:59 +0900

    フォームの項目を追加

--- app/models/idol.rb
+++ app/models/idol.rb
@@ -2,8 +2,14 @@ class Idol < ActiveRecord::Base
 
   include ActiveModel::Model
 
-  attr_accessor :name, :email, :message
+  attr_accessor :name, :email, :age, :height, :hometown, :station, :school, :production
 
-  validates :name, :presence => {:message => '名前を入力してください'}
-  validates :email, :presence => {:message => 'メールアドレスを入力してください'}
+  validates :name, :presence => {:name => '名前を入力してください'}
+  validates :email, :presence => {:email => 'メールアドレスを入力してください'}
+  validates :age, :presence => {:age => '年齢を入力してください'}
+  validates :height, :presence => {:height => '身長を入力してください'}
+  validates :school, :presence => {:message => '学校名と学年を入力してください'}
+  validates :hometown, :presence => {:hometown => '出身地を入力してください'}
+  validates :station, :presence => {:station => '最寄駅を入力してください'}
+  validates :production, :presence => {:production => 'プロダクションに所属しているかご回答ください'}
 end
