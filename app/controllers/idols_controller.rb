90df199e79c1eae8c5dc38685f3f4902bab1507c
shigeru <shiiiiigeeeee10@gmail.com>
2016-03-05 00:15:20 +0900

    paramsでデータはあるけどデータベースにうまく情報が保存されない

--- app/controllers/idols_controller.rb
+++ app/controllers/idols_controller.rb
@@ -11,12 +11,8 @@ class IdolsController < ApplicationController
 
   def confirm
     # 入力値のチェック
-    @idol = Idol.new(params[:idol])
-    if @idol.valid?
-      # OK。確認画面を表示
-      render :action => 'confirm'
-    else
-      # NG。入力画面を再表示
+    @idol = Idol.new(idol_params)
+    unless @idol.valid?
       render :action => 'entry'
     end
   end
@@ -25,10 +21,43 @@ class IdolsController < ApplicationController
 
   def thankyou
     # メール送信
-    @idol = Idol.new(params[:idol])
-    IdolMailer.idol_confirm_email(@idol).deliver
+    # @idol = Idol.create(
+    #   name: params[:idol][:name],
+    #   age: params[:idol][:age],
+    #   school: params[:idol][:school],
+    #   height: params[:idol][:height],
+    #   hometown: params[:idol][:hometown],
+    #   station: params[:idol][:station],
+    #   email: params[:idol][:email],
+    #   date: params[:idol][:date]
+    # )
+    idol = Idol.new
+    idol.name = params[:idol][:name]
+    idol.age = params[:idol][:age]
+    idol.school = params[:idol][:school]
+    idol.height = params[:idol][:height]
+    idol.hometown = params[:idol][:hometown]
+    idol.station = params[:idol][:station]
+    idol.email = params[:idol][:email]
+    idol.production = params[:idol][:production]
+    idol.date = params[:idol][:date]
+    binding.pry
+    idol.save
+    IdolMailer.idol_confirm_email(idol).deliver
 
     # 完了画面を表示
     render :action => 'thankyou'
   end
+
+  private
+    def idol_params
+      binding.pry
+      # params_date_values = []
+      # params[:idol][:date].each do |date|
+      #   params_date_values << date.to_i
+      # end
+      # params[:idol].store(:date, params_date_values)
+      params.require(:idol).permit(:name, :age, :school, :height, :hometown, :station, :email, :production, :date)
+    end
+
 end
--- app/models/idol.rb
+++ app/models/idol.rb
@@ -1,15 +1,15 @@
 class Idol < ActiveRecord::Base
 
-  include ActiveModel::Model
+  # include ActiveModel::Model
 
-  attr_accessor :name, :email, :age, :height, :hometown, :station, :school, :production
+  # attr_accessor :name, :email, :age, :height, :hometown, :station, :school, :production, :date
 
-  validates :name, :presence => {:name => '名前を入力してください'}
-  validates :email, :presence => {:email => 'メールアドレスを入力してください'}
-  validates :age, :presence => {:age => '年齢を入力してください'}
-  validates :height, :presence => {:height => '身長を入力してください'}
-  validates :school, :presence => {:message => '学校名と学年を入力してください'}
-  validates :hometown, :presence => {:hometown => '出身地を入力してください'}
-  validates :station, :presence => {:station => '最寄駅を入力してください'}
-  validates :production, :presence => {:production => 'プロダクションに所属しているかご回答ください'}
+  # validates :name, :presence => {:name => "名前を入力してください"}
+  # validates :email, :presence => {:email => "メールアドレスを入力してください"}
+  # validates :age, :presence => {:age => "年齢を入力してください"}
+  # validates :height, :presence => {:height => "身長を入力してください"}
+  # validates :hometown, :presence => {:hometown => "出身地を入力してください"}
+  # validates :station, :presence => {:station => "最寄駅を入力してください"}
+  # validates :production, :presence => {:production => "プロダクションに所属しているかご回答ください"}
+  # validates :date, :presence => {:date => "説明会に参加できる日程を選択してください"}
 end
